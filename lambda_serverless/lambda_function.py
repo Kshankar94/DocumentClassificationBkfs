072649aca4df175bfa6a202ca7e6ac8c66cd2f06
Kripa Shankar <43283409+Kshankar94@users.noreply.github.com>
2020-11-07 22:45:50 -0600

    updated lambda_function

--- lambda_serverless/lambda_function.py
+++ lambda_serverless/lambda_function.py
@@ -5,6 +5,7 @@ import json
 
 import csv
 
+# client to access sagemaker resources
 sagemaker = boto3.client('sagemaker')
 ENDPOINT_NAME = os.environ['ENDPOINT_NAME']
 runtime= boto3.client('runtime.sagemaker')
@@ -25,14 +26,9 @@ def lambda_handler(event, context):
     payload = data['body']
     
     result_object = {}
-    
-    if(not payload):
-        response_object = response_object_creation()
-        response_object['body'] = 'Insufficient data'
-        return response_object
         
-      
-    if(not event['key']):
+    # if a request doesn't request for a csv to be uploaded, we return the prediction for the text that's provided in a text area (UI) 
+    if('key' in event and not event['key']):
         response = runtime.invoke_endpoint(EndpointName= ENDPOINT_NAME ,Body= payload, ContentType='text/csv', Accept = 'application/json')
         result = json.loads(response['Body'].read().decode())
         response_object = response_object_creation()
@@ -40,13 +36,15 @@ def lambda_handler(event, context):
         return response_object
     
         
-    
+    # boto helps to access s3 bucket
     s3 = boto3.client('s3')
     csv1_data = s3.get_object(Bucket='sagemaker-us-east-2-326300720983', Key= event['key'])
     
     lines = csv1_data['Body'].read().decode("utf-8")
     
     payload_full = ''
+    
+    #converts the csv to a csv string for a whole csv file prediction
     for line in lines.splitlines():
         payload_full+=line
         payload_full+=str('\n')
